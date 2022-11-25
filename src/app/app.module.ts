dc7d56f095a2acd9cb3fbd4e934a2358978b66f7
Radostin <radstatan@gmail.com>
2022-11-25 20:43:28 +0200

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { ThemeModule } from './theme/theme.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
+    ThemeModule,
+    AuthModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// @Component({
+//   selector: 'app-test',
+//   template:'<div>HELLO</div>'
+// })
+
+// export class TestCmp {
+
+//}
+
+// @NgModule({
+//   declarations:[
+//     TestCmp,
+//   ],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// class MyModule {
+//   static withProviders(): ModuleWithProviders<MyModule> {
+//     return {
+//       ngModule: MyModule,
+//       providers: [
+
+//       ]
+//     }
+//   }
+// }
